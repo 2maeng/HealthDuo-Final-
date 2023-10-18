17a6ba62908840b23a1cf6b0ec390c360f14a9df
2maeng <vbn3701@gmail.com>
2023-10-18 17:38:30 +0900

    first commit

---
+++ src/main/java/com/spring/view/controller/ChatController.java
@@ -0,0 +1,16 @@
+//package com.spring.view.controller;
+//
+//import org.springframework.stereotype.Controller;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestMethod;
+//
+//@Controller
+//public class ChatController {
+//
+//	@RequestMapping(value = "/chat.do", method = { RequestMethod.POST })
+//	public String chat () {
+//
+//		return "redirect:chatingRoomPage.jsp";
+//	}
+//
+//}
